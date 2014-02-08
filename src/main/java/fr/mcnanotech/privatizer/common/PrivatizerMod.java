214cdaa8e7b6f4b27ae86fa56ac791bed16248b8
letherman255 <matboularot@yahoo.fr>
2014-02-08 23:08:54 +0100

    added main class

---
+++ src/main/java/fr/mcnanotech/privatizer/common/PrivatizerMod.java
@@ -0,0 +1,16 @@
+package fr.mcnanotech.privatizer.common;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Privatizer", name = "Privatizer", version = "@VERSION@")
+
+public class PrivatizerMod
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+}
